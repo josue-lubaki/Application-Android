0f601ee3959ad3f05a8f30a37f112305cb63a5ad
josue-lubaki <josuelubaki@gmail.com>
2021-02-12 17:32:12 -0500

    implementation menu de navigation

--- projetAndroid/app/src/main/java/ca/josue/projetandroid/MainActivity.java
+++ projetAndroid/app/src/main/java/ca/josue/projetandroid/MainActivity.java
@@ -1,14 +1,64 @@
 package ca.josue.projetandroid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import ca.josue.projetandroid.navigation.add;
+import ca.josue.projetandroid.navigation.home;
+import ca.josue.projetandroid.navigation.message;
+import ca.josue.projetandroid.navigation.profile;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemReselectedListener {
+
+    private Fragment currentFragment = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public void onNavigationItemReselected(@NonNull MenuItem item) {
+        item.setChecked(true);
+        switch(item.getItemId()){
+            case R.id.optionHome:
+                showFragment(home.class);
+                break;
+            case R.id.optionAdd:
+                showFragment(add.class);
+                break;
+            case R.id.optionMessage:
+                showFragment(message.class);
+                break;
+            case R.id.optionProfile:
+                showFragment(profile.class);
+                break;
+
+        }
+    }
+
+    private void showFragment(Class<? extends Fragment> fragment) {
+        try {
+            currentFragment = getSupportFragmentManager().findFragmentByTag(fragment.getName());
+
+            if(currentFragment == null)
+                currentFragment = fragment.newInstance();
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.container, currentFragment, fragment.getName()).commit();
+        }catch(InstantiationException | IllegalAccessException e){
+            e.printStackTrace();
+            Log.i("error","erreur au moment d'instancier fragment");
+        }
     }
 }
---
+++ projetAndroid/app/src/main/java/ca/josue/projetandroid/navigation/profile.java
@@ -0,0 +1,28 @@
+package ca.josue.projetandroid.navigation;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import ca.josue.projetandroid.MainActivity;
+import ca.josue.projetandroid.R;
+
+public class profile extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.profile_layout, container, false);
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        ((MainActivity)context).setTitle("Profile");
+    }
+}
